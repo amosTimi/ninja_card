fe97c7132d16aab7fc8b5564fa15629f0046be79
amostimi <timilehinamos@gmail.com>
2021-04-20 18:32:25 +0100

    First commit for ninja project

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaIdCard(),
+  ));
+}
+
+class NinjaIdCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20.0,30.0,20.0,0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                //backgroundColor: Colors.amberAccent,
+                backgroundImage: AssetImage('assets/amoslee.jpeg'),
+                radius: 50.0,
+                backgroundColor: Colors.transparent,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[700],
+            ),
+            Text('NAME',
+            style: TextStyle(
+              color: Colors.grey[400],
+              letterSpacing: 2.0,
+            )),
+            SizedBox(height: 10.0),
+            Text('Amos-Lee',
+            style: TextStyle(
+              color: Colors.amberAccent,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+          )),
+            SizedBox(height: 20.0),
+            Text('Current Ninja Status',
+                style: TextStyle(
+                  color: Colors.grey[400],
+                  letterSpacing: 2.0,
+                )),
+            SizedBox(height: 10.0),
+            Text('06',
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email_rounded,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text('amos-lee@ninja.co.uk',
+                style: TextStyle(
+                  color: Colors.grey[400],
+                  letterSpacing: 1.0,
+                ))
+              ]
+            )
+          ]
+        )
+      ),
+    );
+  }
+}
