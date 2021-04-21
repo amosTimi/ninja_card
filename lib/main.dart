fa68b3f8f6025a4ca7360b8cc434981f78b88b6a
amostimi <timilehinamos@gmail.com>
2021-04-21 10:17:12 +0100

    Converted to stateful widget and added floatingActionButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() {
   ));
 }
 
-class NinjaIdCard extends StatelessWidget {
+class NinjaIdCard extends StatefulWidget {
+  @override
+  _NinjaIdCardState createState() => _NinjaIdCardState();
+}
+
+class _NinjaIdCardState extends State<NinjaIdCard> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +21,16 @@ class NinjaIdCard extends StatelessWidget {
         title: Text('Ninja ID Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[800],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState((){
+            ninjaLevel += 1;
+          });
+        },
+        backgroundColor: Colors.grey[500],
+        child: Icon(Icons.add,
+        color: Colors.grey[800]),
       ),
       body: Container(
         padding: EdgeInsets.fromLTRB(20.0,30.0,20.0,0.0),
@@ -52,7 +68,7 @@ class NinjaIdCard extends StatelessWidget {
                   letterSpacing: 2.0,
                 )),
             SizedBox(height: 10.0),
-            Text('06',
+            Text('$ninjaLevel',
                 style: TextStyle(
                   color: Colors.amberAccent,
                   fontSize: 30.0,
